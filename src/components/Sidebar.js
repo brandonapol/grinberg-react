e69958d6bae4d891e84f86de7a781d2600f878b2
Brandon Apol <brandonapol@cedarville.edu>
2022-11-04 06:57:00 -0700

    added page routing

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,15 @@
 import { Navbar } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
+import {NavLink} from "react-router-dom";
 
 export default function Sidebar() {
     return (
         <Navbar sticky="top" className="flex-column Sidebar">
             <Nav.Item>
-                <Nav.Link href="/">Feed</Nav.Link>
+                <Nav.Link as={NavLink} to="/" end>Feed</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/explore">Explore</Nav.Link>
+                <Nav.Link as={NavLink} to="/explore" end>Explore</Nav.Link>
             </Nav.Item>
         </Navbar>
     );
